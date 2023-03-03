8b62754664ad438b130e628b0c9c187dfa22b161
Pedro Carvalho <pedcmartins@gmail.com>
2023-03-03 18:40:26 -0300

    changed an absolute path to relative path

--- python/paymentProcessing.py
+++ python/paymentProcessing.py
@@ -21,7 +21,7 @@ def launchPaymentProcessing(price, paymentMethod):
     print(paymentMethodInput)
     
     payment_sh_command = [
-        "/home/pi/Desktop/sistema_pagamento/coffea_proj_auto/plugpag_integration/rpi_plugpag_dev/output/payment_request_plugpag",
+        "/../plugpag_integration/rpi_plugpag_dev/output/payment_request_plugpag",
         "COM0",
         paymentMethodInput,
         "1",
