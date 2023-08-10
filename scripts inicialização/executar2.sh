53c04f7701902bf7067dfa178520c13417b0fc8c
Pedro Carvalho Martins <pedro.carvalho-martins@outlook.com>
2023-08-10 20:06:37 -0300

    adicionados scripts de inicilização do raspberry pi

---
+++ scripts inicialização/executar2.sh
@@ -0,0 +1,13 @@
+#sudo rfcomm release all
+#sudo rfcomm bind /dev/rfcomm0 70:4A:0E:D4:E1:D7 1
+#sudo chmod 777 /dev/rfcomm0
+cd coffea_proj_auto
+cd python
+python startup_BT_connection.py
+sleep 4
+nohup python launch_background.py &
+sleep 1
+while true
+do
+	python main.py
+done
