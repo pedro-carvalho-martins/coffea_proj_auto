4c1f2f738c4315a5b4d19c77432ab85c6dcbcf74
Pedro Carvalho <pedcmartins@gmail.com>
2024-08-16 12:36:40 -0300

    Added management of backup log files

--- python/logTransmissionProcess.py
+++ python/logTransmissionProcess.py
@@ -106,7 +106,7 @@ def verify_json_files(json_folder):
         print(client_size)
 
         if server_size == client_size: # Works on Raspberry Pi. Uncomment it after Windows testing
-        # if server_size == server_size: # Make this line uncommented only for Windows testing
+        #if server_size == server_size: # Make this line uncommented only for Windows testing
             request = {
                 "type": "acknowledge_file",
                 "param1": client_name,
--- python/rwLogCSV.py
+++ python/rwLogCSV.py
@@ -1,11 +1,61 @@
-from datetime import datetime
+import datetime
 import csv
 import json
 import os
+import shutil
 
 from shared_resource import file_lock
 
 import rwSystemID
+
+
+def delete_old_csv_backup_files():
+    # Get the current year
+    current_year = datetime.datetime.now().year
+
+    # Get all CSV files in the backup directory
+    csv_files = [f for f in os.listdir('./log_files/csv_full_logs_backup/') if f.endswith('.csv')]
+
+    for csv_file in csv_files:
+        if csv_file.startswith('backup_full_client_log_Y'):
+            # Extract the year from the file name
+            year = int(csv_file[ len('backup_full_client_log_Y') + 1 : len('backup_full_client_log_Y') + 5 ])
+
+            # If the file is older than last year, delete it
+            if year < current_year - 1:
+                os.remove(os.path.join('./log_files/csv_full_logs_backup/', csv_file))
+                print(f"Deleted old backup CSV file: {csv_file}")
+
+
+def prepare_and_check_current_year_week_backup_csv_path():
+
+    datetime_now_isocalendar = datetime.datetime.now().isocalendar()
+    year_week_string = 'Y' + str(datetime_now_isocalendar[0]) + '_W' + str(datetime_now_isocalendar[1]).zfill(2)
+
+    current_yearweek_csv_file_path = "./log_files/csv_full_logs_backup/backup_full_client_log_" + year_week_string + ".csv"
+
+    # Check if the CSV file exists
+    if not os.path.isfile(current_yearweek_csv_file_path):
+
+        # Define the model CSV file path
+        model_csv_path = "./log_files/log_header_backup_model.csv"
+
+        # Check if the model CSV file exists
+        if os.path.isfile(model_csv_path):
+            # Copy the model CSV file to the specified path
+            shutil.copyfile(model_csv_path, current_yearweek_csv_file_path)
+            print(f"'{current_yearweek_csv_file_path}' created from '{model_csv_path}'.")
+
+            # Delete old backup files
+            delete_old_csv_backup_files()
+
+        else:
+            print(f"Model CSV file '{model_csv_path}' does not exist.")
+            # Error treatment pending
+
+    return current_yearweek_csv_file_path
+
+
 
 def writeCSV(tipo_registro, valor_venda_str, metodo_pag, etapa_erro, classe_erro, descricao_erro):
 
@@ -15,14 +65,13 @@ def writeCSV(tipo_registro, valor_venda_str, metodo_pag, etapa_erro, classe_erro
     descricao_erro = descricao_erro.replace("'", "-")
 
     # Obtains the datetime at the moment of the log register
-    datetime_register_str = str(datetime.now())[0:19]
+    datetime_register_str = str(datetime.datetime.now())[0:19]
 
     # Obtains the name of the system (system ID)
     nome_sistema = rwSystemID.readSystemID()
 
     # Define the CSV filename
     filename_csv_tmp = "./log_files/tmp_log_client.csv"
-    filename_csv_backup = "./log_files/backup_full_log_client.csv"
 
     # WRITE TO CSV TO BE PASSED TO SERVER
     with file_lock:
@@ -32,7 +81,11 @@ def writeCSV(tipo_registro, valor_venda_str, metodo_pag, etapa_erro, classe_erro
             csvwriter.writerow([datetime_register_str, nome_sistema, tipo_registro, valor_venda_str,
                                 metodo_pag, etapa_erro, classe_erro[0:50], descricao_erro[0:100]])
 
-    # WRITE TO BACKUP CSV - for TESTING PURPOSES ONLY
+
+    # WRITE TO BACKUP CSV
+
+    filename_csv_backup = prepare_and_check_current_year_week_backup_csv_path()
+
     with file_lock:
     # Open the file in append mode
         with open(filename_csv_backup, 'a', newline='') as csvfile:
